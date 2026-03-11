d220dae31f9ce1cf43a6f4eef06397d89ec87527
dedbabay <alexander.abdoullaev@gmail.com>
2026-03-12 01:43:22 +0300

    Create 0014_LongestPrefix.go
    https://leetcode.com/problems/longest-common-prefix/description/

---
+++ 0014_LongestPrefix.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func longestCommonPrefix(strs []string) string {
+
+	cp := []byte{}
+
+	fs := strs[0]
+	for fsi := 0; fsi < len(fs); fsi++ {
+		for i := 1; i < len(strs); i++ {
+			if fs[fsi]-strs[i][fsi] != 0 || fsi >= len(strs[i])-1 {
+				return string(cp)
+			}
+		}
+		cp = append(cp, fs[fsi])
+	}
+	return string(cp)
+}
+
+func main() {
+	s1 := []string{"flower", "flow", "flossome"}
+	s2 := []string{"peter", "bruno"}
+
+	fmt.Println(longestCommonPrefix(s1))
+	fmt.Println(longestCommonPrefix(s2))
+}
