6dfef4fd3ac09c1d073e5402d5859ab1d3962b4d
dedbabay <alexander.abdoullaev@gmail.com>
2026-03-18 23:57:33 +0300

    Create 0020_valid_brackets.go
    https://leetcode.com/problems/valid-parentheses/

---
+++ 0020_valid_brackets.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isValid(s string) bool {
+
+	a := []rune{}
+
+	for _, c := range s {
+		if c == '[' || c == '{' || c == '(' {
+			a = append(a, c)
+		} else {
+			l := a[len(a)-1]
+			if (c == ']' && l != '[') ||
+				(c == ')' && l != '(') ||
+				(c == '}' && l != '{') {
+				return false
+			}
+			a = a[:len(a)-1]
+		}
+	}
+	return len(a) == 0
+}
+
+func main() {
+	s1 := "()"
+	fmt.Println(s1, isValid(s1))
+
+	s2 := "({[]})"
+	fmt.Println(s2, isValid(s2))
+
+	s3 := "([)])"
+	fmt.Println(s3, isValid(s3))
+
+}
